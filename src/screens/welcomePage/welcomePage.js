7068d6a48e4e136d906f7ebf63435da4d2d52efb
unknown <yessinejawa@gmail.com>
2022-07-18 16:28:39 +0200

    adding yessine file

--- src/screens/welcomePage/welcomePage.js
+++ src/screens/welcomePage/welcomePage.js
@@ -1,12 +1,35 @@
-import React from "react";
-import { StyleSheet, View, Text, Button, Image } from "react-native";
+import React, { useState } from "react";
+import { Animated, View, Text, Button, Image } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { windowWidth, windowHeight } from "../../utils/dimensions";
 import { welcomePageStyle } from "./welcomePageStyle";
 const WelcomePage = ({ navigation }) => {
+  const value = useState(new Animated.ValueXY({ x: 0, y: 0 }))[0];
+  const moveTheBall = () => {
+    console.log("first");
+    Animated.timing(value, {
+      toValue: { x: 200, y: 200 },
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+  };
   return (
     <View style={welcomePageStyle.container}>
       <View style={welcomePageStyle.firstContainer}>
+        <View>
+          <Animated.View>
+            <View
+              style={{
+                width: 200,
+                height: 200,
+                backgroundColor: "red",
+                borderRadius: 20,
+              }}
+            />
+          </Animated.View>
+          <Button title="ahah" onPress={moveTheBall}>
+            <Text>Click here</Text>
+          </Button>
+        </View>
         <Text style={welcomePageStyle.organizeMeText}>Organize Me</Text>
         <Image
           source={require("../../assets/images/welcomePage.png")}
