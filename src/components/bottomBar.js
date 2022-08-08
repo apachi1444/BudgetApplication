9a92c51ddf21451b99fa023b83e382a7db506d72
unknown <yessine.jawa@neoxia.com>
2022-08-08 12:05:53 +0200

    working on the animation bottom bar

--- src/components/bottomBar.js
+++ src/components/bottomBar.js
@@ -1,134 +1,14 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React, { useEffect, useRef } from "react";
-import {
-  StyleSheet,
-  Button,
-  Text,
-  TouchableOpacity,
-  View,
-  ScrollView,
-} from "react-native";
-import Icon, { Icons } from "./icon";
-import Settings from "../screens/settings/settings";
-import COLORS from "./../consts/color";
-import ProfileUser from "../screens/profile/profile";
-import { SIZES } from "../consts/theme";
-import HistoryStack from "../routes/historyStack";
-import Guide_Stack from "../routes/guide_50_30_20_Stack";
-import Add from "./add/add";
-import Modal from "react-native-modal";
+import { StyleSheet, Text, View } from "react-native";
+import { TabArr } from "../consts/tab";
 
-const TabArr = [
-  {
-    route: "Home",
-    label: "Home",
-    type: Icons.MaterialCommunityIcons,
-    icon: "home",
-    component: ProfileUser,
-  },
-  {
-    route: "History",
-    label: "History",
-    type: Icons.MaterialCommunityIcons,
-    icon: "briefcase",
-    component: HistoryStack,
-  },
-  {
-    route: "Add",
-    label: "Add",
-    type: Icons.Feather,
-    icon: "plus",
-    component: Add,
-  },
-  {
-    route: "guide",
-    label: "Guide",
-    type: Icons.MaterialCommunityIcons,
-    icon: "google-assistant",
-    component: Guide_Stack,
-  },
-  {
-    route: "Settings",
-    label: "Settings",
-    type: Icons.MaterialCommunityIcons,
-    icon: "cog",
-    component: Settings,
-  },
-];
+import { TabButton } from "./tabButton";
+import { TabIcon } from "./tabIcon";
+import COLORS from "../consts/color";
+import { SIZES } from "../consts/theme";
 
 const Tab = createBottomTabNavigator();
-
-const TabButton = (props) => {
-  const { item, accessibilityState } = props;
-  const focused = accessibilityState.selected;
-
-  const [isModalVisible, setIsModalVisible] = React.useState(false);
-
-  const handleModal = () => setIsModalVisible(() => !isModalVisible);
-  useEffect(() => {
-    if (focused) {
-      // here we will do the animation of the three buttons to add a spending or an income
-    } else {
-    }
-  }, [focused]);
-
-  return (
-    <TouchableOpacity style={styles.container} onPress={handleModal}>
-      <View style={styles.container}>
-        <View style={{ backgroundColor: COLORS.DELETEBUTTONRED }}>
-          <Modal
-            presentationStyle="overFullScreen"
-            isVisible={isModalVisible}
-            style={{
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <View
-              style={{
-                width: "100%",
-                backgroundColor: "transparent",
-                flex: 1,
-              }}
-            >
-              <Add />
-              <Button title="Done" onPress={handleModal} />
-            </View>
-          </Modal>
-        </View>
-        <View style={styles.buttonAdd}>
-          <View style={styles.circle} />
-          <Icon type={item.type} name={item.icon} color={COLORS.BLACK} />
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
-};
-
-const TabIcon = (props) => {
-  const { item, focused } = props;
-  return (
-    <View style={styles.iconAndLabelView}>
-      <Icon
-        type={item.type}
-        name={item.icon}
-        style={styles.icon}
-        color={focused ? COLORS.FOCUSEDTAB : COLORS.PRIMARY}
-      />
-      <Text
-        style={[
-          {
-            color: focused ? COLORS.FOCUSEDTAB : COLORS.PRIMARY,
-            fontWeight: "700",
-          },
-        ]}
-      >
-        {item.label}
-      </Text>
-    </View>
-  );
-};
-
 export default function BottomBar() {
   return (
     <Tab.Navigator
@@ -176,12 +56,12 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 10,
   },
   buttonAdd: {
-    width: SIZES.BASE * 8,
-    height: SIZES.BASE * 8,
+    width: SIZES.BASE * 9,
+    height: SIZES.BASE * 9,
     borderRadius: SIZES.BASE * 4,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: SIZES.BASE * 6,
+    marginBottom: SIZES.BASE * 8,
   },
   circle: {
     ...StyleSheet.absoluteFillObject,
